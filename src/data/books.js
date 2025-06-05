4334ae69f33ef0f2073cfe4ddfaf8a5432fd7a7b
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-06-05 18:15:08 +0100

    feat: Most pages read

--- src/data/books.js
+++ src/data/books.js
@@ -63,7 +63,7 @@ export const books = [
         cover: "/images/books/a-psicologia-financeira.webp",
         title: "A Psicologia Financeira",
         author: "Morgan Housel",
-        completed: (78 / 208),
+        completed: (92 / 208),
     },
     {
         cover: "/images/books/o-caminho-para-a-felicidade.webp",
