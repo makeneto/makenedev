44f715869611be9992febdab0938ff07b7829266
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-07-28 20:13:19 +0100

    feat: read more pages

--- src/data/books.js
+++ src/data/books.js
@@ -57,7 +57,7 @@ export const books = [
         cover: "/assets/books/1-ano-em-12-semanas.webp",
         title: "1 Ano em 12 Semanas",
         author: "Brian P. Moran",
-        completed: (153 / 194),
+        completed: (159 / 194),
     },
     {
         cover: "/assets/books/a-psicologia-financeira.webp",
