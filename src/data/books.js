40beb1521cf7fdab07d5ebe0cb50eef5b1fd3345
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-08-02 20:52:29 +0100

    feat: read more pages

--- src/data/books.js
+++ src/data/books.js
@@ -51,7 +51,7 @@ export const books = [
         cover: "/assets/books/essencialismo.webp",
         title: "Essencialismo",
         author: "Greg Mckeown",                         
-        completed: (0 / 227),
+        completed: (10 / 227),
     }, 
     {
         cover: "/assets/books/1-ano-em-12-semanas.webp",
