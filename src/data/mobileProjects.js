faaa943bf8ef600d972579f1ac06432bb87694f5
Makene Neto <makeneto17@gmail.com>
2025-08-18 21:01:06 +0100

    perf: convert shots to webp format

--- src/data/mobileProjects.js
+++ src/data/mobileProjects.js
@@ -3,32 +3,32 @@ export const mobileProjects = [
         id: 'app1',
         logo: '/assets/projects/finloop-logo.webp',
         title: 'Finloop',
-        description: 'Your finances in constant evolution',
+        description: 'Smart savings, real progress',
         category: 'Finance',
         screenshots: [
             {
                 id: 's1',
-                imageUrl: '/assets/projects/finloop1.jpg',
+                imageUrl: '/assets/projects/finloop1.webp',
             },
             {
                 id: 's2',
-                imageUrl: '/assets/projects/finloop2.jpg',
+                imageUrl: '/assets/projects/finloop2.webp',
             },
             {
                 id: 's3',
-                imageUrl: '/assets/projects/finloop3.jpg',
+                imageUrl: '/assets/projects/finloop3.webp',
             },
             {
                 id: 's4',
-                imageUrl: '/assets/projects/finloop4.jpg',
+                imageUrl: '/assets/projects/finloop4.webp',
             },
             {
                 id: 's5',
-                imageUrl: '/assets/projects/finloop5.jpg',
+                imageUrl: '/assets/projects/finloop5.webp',
             },
             {
                 id: 's6',
-                imageUrl: '/assets/projects/finloop6.jpg',
+                imageUrl: '/assets/projects/finloop6.webp',
             },
         ],
     },
@@ -36,24 +36,24 @@ export const mobileProjects = [
         id: 'app2',
         logo: '/assets/projects/goals-logo.webp',
         title: 'Goals',
-        description: 'Your progress begins with a decision',
+        description: 'Focus. Act. Achieve',
         category: 'Productivity',
         screenshots: [
             {
                 id: 's1',
-                imageUrl: '/assets/projects/goals1.jpg',
+                imageUrl: '/assets/projects/goals1.webp',
             },
             {
                 id: 's2',
-                imageUrl: '/assets/projects/goals2.jpg',
+                imageUrl: '/assets/projects/goals2.webp',
             },
             {
                 id: 's3',
-                imageUrl: '/assets/projects/goals3.jpg',
+                imageUrl: '/assets/projects/goals3.webp',
             },
             {
                 id: 's4',
-                imageUrl: '/assets/projects/goals4.jpg',
+                imageUrl: '/assets/projects/goals4.webp',
             },
         ],
     },
