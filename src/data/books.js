d50b00f7e4c7c737e7b5dc668a041a69f889daf9
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-11-02 15:22:25 +0100

    chore update completion status for a book

--- src/data/books.js
+++ src/data/books.js
@@ -57,7 +57,7 @@ export const books = [
         cover: "/assets/books/como-fazer-amigos-e-influenciar-pessoas.webp",
         title: "How to make friends and influence people",
         author: "Dale Carnegie",
-        completed: (156 / 284),
+        completed: (164 / 284),
     },
     {
         cover: "/assets/books/a-sutil-arte-de-ligar-o-foda-se.webp",
