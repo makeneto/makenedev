0c246bc93f7c3a0d7a564061759378a492560c60
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-11-12 20:24:10 +0100

    chore: read more pages

--- src/data/books.js
+++ src/data/books.js
@@ -57,7 +57,7 @@ export const books = [
         cover: "/assets/books/como-fazer-amigos-e-influenciar-pessoas.webp",
         title: "How to make friends and influence people",
         author: "Dale Carnegie",
-        completed: (198 / 284),
+        completed: (212 / 284),
     },
     {
         cover: "/assets/books/a-sutil-arte-de-ligar-o-foda-se.webp",
