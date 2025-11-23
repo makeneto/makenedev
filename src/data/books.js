f231423767a9237122e83269583ee83cc20fff61
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-11-23 18:15:53 +0100

    chore: read more pages

--- src/data/books.js
+++ src/data/books.js
@@ -51,7 +51,7 @@ export const books = [
         cover: "/assets/books/startwithwhy.webp",
         title: "Start with why",
         author: "Simon Sinek",
-        completed: (88 / 169),
+        completed: (99 / 169),
     },
     {
         cover: "/assets/books/como-fazer-amigos-e-influenciar-pessoas.webp",
