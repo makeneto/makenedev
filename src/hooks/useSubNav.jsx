c576c83c73b87976f2f8d12925c76a347ffc100d
Makene Neto <130509472+makeneto@users.noreply.github.com>
2026-02-08 15:43:15 +0000

    chore: update makeneResume link in useSubNav hook

--- src/hooks/useSubNav.jsx
+++ src/hooks/useSubNav.jsx
@@ -2,7 +2,7 @@ import { useCloseSkills } from '../helpers/showSkills'
 
 export default function useSubNav() {
     const linkedInLink = 'https://linkedin.com/in/makene-neto'
-    const makeneResume = 'https://suave-woodwind-741.notion.site/Makene-Neto-1e9c66691455804595b3d0c254a66754'
+    const makeneResume = 'https://suave-woodwind-741.notion.site/Makene-Neto-301c6669145580e5abd7dfda35148db0?source=copy_link'
     const handleCloseSkills = useCloseSkills()
 
     return { linkedInLink, makeneResume, handleCloseSkills }
