cb512b9cbd5e88128a22ebef43ad5668d692fd34
Makene Neto <makeneto17@gmail.com>
2026-05-23 14:21:14 +0100

    refactor: delete skill section from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import Header from "../components/Header"
 import QuoteOfTheDay from "../components/QuoteOfTheDay"
-import Skills from "../components/Skills"
 
 export default function Home() {
-    return (
-        <>
-            <Header />
-            <QuoteOfTheDay />
-            <Skills />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <QuoteOfTheDay />
+    </>
+  )
 }
