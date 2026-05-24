60215f569ae70cb3cdc4e119a0861c53ae4b6e38
Makene Neto <makeneto17@gmail.com>
2026-05-24 20:20:48 +0100

    fix: bring router context to main.jsx file

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 
 import "./css/style.css"
+import "./css/index.css"
+
+import Navbar from "./components/Navbar.tsx"
+import { BrowserRouter } from "react-router-dom"
+import Footer from "./components/Footer.tsx"
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <App />
+      <Footer />
+    </BrowserRouter>
+  </StrictMode>,
 )
