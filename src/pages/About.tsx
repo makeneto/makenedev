5dcbeee212e478ea4691bc9835017a35445ee6a0
Makene Neto <makeneto17@gmail.com>
2026-05-25 14:04:20 +0100

    feat: render WorkExperience component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,13 @@
-import AboutDescription from "../components/about/AboutDescription"
-import Faq from "../components/about/Faq"
 import DefaultHeader from "../components/headers/DefaultHeader"
+import AboutDescription from "../components/about/AboutDescription"
+import WorkExperience from "../components/about/WorkExperience"
+import Faq from "../components/about/Faq"
 
 export default function About() {
   return (
     <div>
       <DefaultHeader title="About me" component={<AboutDescription />} />
-
+      <WorkExperience />
       <Faq />
     </div>
   )
