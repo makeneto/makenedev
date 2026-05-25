9365643e3608a16e2859a6380a828c86a99850cb
Makene Neto <makeneto17@gmail.com>
2026-05-25 14:03:51 +0100

    feat: develop work experience section

---
+++ src/components/about/WorkExperience.tsx
@@ -0,0 +1,11 @@
+import Experiences from "./Experiences"
+import Profile from "./Profile"
+
+export default function WorkExperience() {
+  return (
+    <section className="work-experience">
+      <Profile />
+      <Experiences />
+    </section>
+  )
+}
