1e46c0ebc9b20f11d8ce3b7edb225e7fd94eb0ac
Makene Neto <makeneto17@gmail.com>
2026-05-25 14:04:57 +0100

    chore: create contact links array

--- src/data/links.ts
+++ src/data/links.ts
@@ -50,3 +50,36 @@ export const socialLinks = [
     link: "https://instagram.com/makeneto",
   },
 ]
+
+export const contacts = [
+  {
+    label: "Email",
+    href: "mailto:makenedev@gmail.com",
+    display: "makenedev@gmail.com",
+  },
+  {
+    label: "WhatsApp",
+    href: "https://wa.me/244945336003?text=Hi%20Makene!%20I%20came%20across%20your%20portfolio%20and%20would%20love%20to%20get%20in%20touch",
+    display: "+244 945 536 003",
+  },
+  {
+    label: "Portfolio",
+    href: "https://makenedev.vercel.app",
+    display: "makenedev.com",
+  },
+]
+
+export const profileLinks = [
+  {
+    label: "Github",
+    link: "https://github.com/makeneto",
+  },
+  {
+    label: "LinkedIn",
+    link: "https://linkedin.com/in/makene-neto",
+  },
+  {
+    label: "Email",
+    link: "mailto:makenedev@gmail.com",
+  },
+]
