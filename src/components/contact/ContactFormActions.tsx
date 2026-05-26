6b0356b888a787fe91926f6e5133b648264cb3ef
Makene Neto <makeneto17@gmail.com>
2026-05-26 16:34:59 +0100

    feat: develop form button actions

---
+++ src/components/contact/ContactFormActions.tsx
@@ -0,0 +1,39 @@
+import { Loader2, Send } from "lucide-react"
+
+import { Button } from "../ui/button"
+
+interface Props {
+  loading: boolean
+  disabled: boolean
+  onSubmit: () => void
+}
+
+export function ContactFormActions({ loading, disabled, onSubmit }: Props) {
+  return (
+    <Button
+      disabled={disabled || loading}
+      onClick={onSubmit}
+      className="
+        mt-8
+        py-5
+        w-full
+        rounded-full
+        bg-white
+        text-black
+        hover:bg-zinc-200
+        disabled:pointer-events-none
+        disabled:cursor-not-allowed
+        disabled:opacity-40
+      "
+    >
+      {loading ? (
+        <Loader2 className="animate-spin" />
+      ) : (
+        <>
+          Submit Form
+          <Send size={10} />
+        </>
+      )}
+    </Button>
+  )
+}
