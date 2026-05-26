79186528cbed93cc4636d2ff0cb567c07ebff302
Makene Neto <makeneto17@gmail.com>
2026-05-26 16:34:35 +0100

    feat: develop contact form

---
+++ src/components/contact/ContactForm.tsx
@@ -0,0 +1,37 @@
+import { useContactForm } from "../../hooks/useContactForm"
+import { ContactFormActions } from "./ContactFormActions"
+import { ContactFormFields } from "./ContactFormFields"
+import { ContactFormMessage } from "./ContactFormMessage"
+import { ContactFormServices } from "./ContactFormServices"
+
+export function ContactForm() {
+  const { form, loading, isValid, errors, updateField, handleSubmit } =
+    useContactForm()
+
+  return (
+    <div className="contact-form">
+      <ContactFormFields
+        form={form}
+        errors={errors}
+        updateField={updateField}
+      />
+
+      <ContactFormServices
+        services={form.services}
+        onChange={(value) => updateField("services", value)}
+      />
+
+      <ContactFormMessage
+        value={form.message}
+        error={errors.message}
+        onChange={(e) => updateField("message", e.target.value)}
+      />
+
+      <ContactFormActions
+        loading={loading}
+        disabled={!isValid}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  )
+}
