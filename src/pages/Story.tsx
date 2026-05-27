71431b352a49798937203294c11fbe0a5e99ce70
Makene Neto <makeneto17@gmail.com>
2026-05-27 22:18:24 +0100

    feat: render timeline and storyResume in story page

--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -1,10 +1,17 @@
+import React from "react"
 import DefaultHeader from "../components/headers/DefaultHeader"
+import StoryResume from "../components/story/StoryResume"
+import Timeline from "../components/story/Timeline"
 
 export default function Story() {
   return (
-    <DefaultHeader
-      title="My Journey"
-      description="A career isn't a single road. It's tracks running in parallel — some loud, some quiet, all moving at the same time. Engineering, business, design. Not one after the other. All at once."
-    />
+    <React.Fragment>
+      <DefaultHeader
+        title="My Journey"
+        description="A career isn't a single road. It's tracks running in parallel — some loud, some quiet, all moving at the same time. Engineering, business, design. Not one after the other. All at once."
+      />
+      <Timeline />
+      <StoryResume />
+    </React.Fragment>
   )
 }
