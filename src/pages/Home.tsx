64af9c559f207038c6da8c77268ad00ae3126438
Makene Neto <makeneto17@gmail.com>
2026-05-27 22:17:41 +0100

    feat:  apply react fragment and remove margin-top from the contact page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,23 @@
-import { ContactSection } from "../components/contact/ContactSection"
+import React from "react"
+import ContactSection from "../components/contact/ContactSection"
 import HomeHeader from "../components/headers/HomeHeader"
 import QuoteOfTheDay from "../components/QuoteOfTheDay"
+import ShowcaseSection from "../components/showcase-section/ShowcaseSection"
+import { works } from "../data/works"
 
 export default function Home() {
   return (
-    <>
+    <React.Fragment>
       <HomeHeader />
       <QuoteOfTheDay />
-      <ContactSection />
-    </>
+      <ShowcaseSection
+        title="Last Projects"
+        viewAllHref="/work"
+        items={works}
+        isHomePage
+      />
+
+      <ContactSection className="mt-80" />
+    </React.Fragment>
   )
 }
