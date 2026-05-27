cb23037e54b8ed8c7dd138d58b11a0c2bf14954c
Makene Neto <makeneto17@gmail.com>
2026-05-27 22:19:11 +0100

    feat: render showcase section and WorkResume in work page

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,10 +1,20 @@
+import React from "react"
 import DefaultHeader from "../components/headers/DefaultHeader"
+import ShowcaseSection from "../components/showcase-section/ShowcaseSection"
+import WorkResume from "../components/WorkResume"
+import { works } from "../data/works"
 
 export default function Work() {
   return (
-    <DefaultHeader
-      title="My Work"
-      description="Some things you see and move on. Others stop you. Make you look twice. Make you wonder how it was made. That's the only kind of work worth doing."
-    />
+    <React.Fragment>
+      <DefaultHeader
+        title="My Work"
+        description="Some things you see and move on. Others stop you. Make you look twice. Make you wonder how it was made. That's the only kind of work worth doing."
+      />
+
+      <ShowcaseSection title="Last Works" items={works} />
+
+      <WorkResume />
+    </React.Fragment>
   )
 }
