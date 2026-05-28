5236812d1e35dada733e337f01bfb57ee8d97bd0
Makene Neto <makeneto17@gmail.com>
2026-05-28 20:46:53 +0100

    refactor(about): comment out unused components in About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,13 +2,15 @@ import DefaultHeader from "../components/headers/DefaultHeader"
 import AboutDescription from "../components/about/AboutDescription"
 import WorkExperience from "../components/about/WorkExperience"
 import Faq from "../components/about/Faq"
+import StackSection from "../components/stack/StackSection"
 
 export default function About() {
   return (
     <div>
-      <DefaultHeader title="About me" component={<AboutDescription />} />
-      <WorkExperience />
-      <Faq />
+      {/* <DefaultHeader title="About me" component={<AboutDescription />} /> */}
+      <StackSection />
+      {/* <WorkExperience /> */}
+      {/* <Faq /> */}
     </div>
   )
 }
