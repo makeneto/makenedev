61c8cc2e5e3399200ad34a89770fc4f49ce9fcc1
Makene Neto <makeneto17@gmail.com>
2026-05-29 16:29:04 +0100

    refactor(showcase): develop StackCard component

---
+++ src/components/stack/StackCard.tsx
@@ -0,0 +1,38 @@
+import { LinkIcon } from "lucide-react"
+import { Link } from "react-router-dom"
+import { useFindStack } from "../../hooks/useFindStack"
+
+interface StackProps {
+  children: React.ReactNode
+  name: string
+}
+
+export default function StackCard({ children, name }: StackProps) {
+  const stack = useFindStack(name)
+
+  if (!stack) return null
+
+  const { category, link } = stack
+
+  return (
+    <div className="stack-card">
+      {children}
+
+      <div className="flex flex-col items-center gap-1.5">
+        <p className="text-sm font-semibold text-center">{name}</p>
+        <span className="border border-zinc-800 rounded-4xl py-0.5 px-2.5 text-xs text-zinc-400 font-mono font-semibold">
+          {category}
+        </span>
+
+        <Link
+          to={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Website"
+        >
+          <LinkIcon size={12} />
+        </Link>
+      </div>
+    </div>
+  )
+}
