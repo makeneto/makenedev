dd5808540ca708255ceeb766b28997e7397c5d7a
Makene Neto <makeneto17@gmail.com>
2026-05-29 16:34:56 +0100

    feat(stack): apply stack filter by name

---
+++ src/components/ui/icons/VercelIcon.tsx
@@ -0,0 +1,11 @@
+import StackCard from "../../stack/StackCard"
+
+export default function VercelIcon() {
+  return (
+    <StackCard name="Vercel">
+      <svg viewBox="0 0 256 222" preserveAspectRatio="xMidYMid">
+        <path fill="#fff" d="m128 0 128 221.705H0z" />
+      </svg>
+    </StackCard>
+  )
+}
