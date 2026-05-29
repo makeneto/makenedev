2d0741dab4a3bb233680c2e1846e2931068c4752
Makene Neto <makeneto17@gmail.com>
2026-05-29 16:30:26 +0100

    refactor(stack): divide the file into components and hooks

--- src/components/stack/StackSection.tsx
+++ src/components/stack/StackSection.tsx
@@ -1,53 +1,18 @@
-import AnthropicIcon from "../ui/icons/AnthropicIcon"
-import ClaudeIcon from "../ui/icons/ClaudeIcon"
-import ExpressIcon from "../ui/icons/ExpressIcon"
-import GithubIcon from "../ui/icons/GithubIcon"
-import GitIcon from "../ui/icons/GitIcon"
-import JavaScriptIcon from "../ui/icons/JavaScriptIcon"
-import MicrosoftIcon from "../ui/icons/MicrosoftIcon"
-import NextIcon from "../ui/icons/NextIcon"
-import NodeIcon from "../ui/icons/NodeIcon"
-import NpmIcon from "../ui/icons/NpmIcon"
-import OpenaiIcon from "../ui/icons/OpenaiIcon"
-import PnpmIcon from "../ui/icons/PnpmIcon"
-import PostgresqlIcon from "../ui/icons/PostgresqlIcon"
-import ReactIcon from "../ui/icons/ReactIcon"
-import ReduxIcon from "../ui/icons/ReduxIcon"
-import SassIcon from "../ui/icons/SassIcon"
-import ShadcnIcon from "../ui/icons/ShadcnIcon"
-import SocketioIcon from "../ui/icons/SocketioIcon"
-import TailwindcssIcon from "../ui/icons/TailwindcssIcon"
-import TypescriptIcon from "../ui/icons/TypescriptIcon"
-import UdemyIcon from "../ui/icons/UdemyIcon"
+import { useStacks } from "../../hooks/useStacks"
+import StackGrid from "./StackGrid"
 import StackHeader from "./StackHeader"
 
 export default function StackSection() {
+  const { sections } = useStacks()
+
   return (
-    <section>
-      <StackHeader title="Front-end" />
-      <div className="stack-grid">
-        <TypescriptIcon />
-        <JavaScriptIcon />
-        <ReactIcon />
-        <NextIcon />
-        <ReduxIcon />
-        <SassIcon />
-        <TailwindcssIcon />
-        <ShadcnIcon />
-        <NodeIcon />
-        <ExpressIcon />
-        <SocketioIcon />
-        <PostgresqlIcon />
-        <ClaudeIcon />
-        <OpenaiIcon />
-        <GitIcon />
-        <GithubIcon />
-        <PnpmIcon />
-        <NpmIcon />
-        <UdemyIcon />
-        <MicrosoftIcon />
-        <AnthropicIcon />
-      </div>
+    <section className="grid gap-25">
+      {sections.map(({ id, title, stacks }) => (
+        <section key={id}>
+          <StackHeader title={title} />
+          <StackGrid stacks={stacks} />
+        </section>
+      ))}
     </section>
   )
 }
