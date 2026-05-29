76367cf364467e4e8da6af763ece6c53a9c47f82
Makene Neto <makeneto17@gmail.com>
2026-05-29 16:36:43 +0100

    refactor(about page): uncomment the components of the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,15 +2,15 @@ import DefaultHeader from "../components/headers/DefaultHeader"
 import AboutDescription from "../components/about/AboutDescription"
 import WorkExperience from "../components/about/WorkExperience"
 import Faq from "../components/about/Faq"
-import StackSection from "../components/stack/StackSection"
+import Timeline from "../components/story/Timeline"
 
 export default function About() {
   return (
     <div>
-      {/* <DefaultHeader title="About me" component={<AboutDescription />} /> */}
-      <StackSection />
-      {/* <WorkExperience /> */}
-      {/* <Faq /> */}
+      <DefaultHeader title="About me" component={<AboutDescription />} />
+      <Timeline />
+      <WorkExperience />
+      <Faq />
     </div>
   )
 }
