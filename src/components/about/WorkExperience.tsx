594a4040e4d07f18b902d1fe74c230db0b608305
Makene Neto <makeneto17@gmail.com>
2026-05-30 18:49:57 +0100

    style: render showcase header above the profile section

--- src/components/about/WorkExperience.tsx
+++ src/components/about/WorkExperience.tsx
@@ -1,11 +1,15 @@
+import ShowcaseHeader from "../showcase-section/ShowcaseHeader"
 import Experiences from "./Experiences"
 import Profile from "./Profile"
 
 export default function WorkExperience() {
   return (
-    <section className="work-experience">
-      <Profile />
-      <Experiences />
+    <section className="experience-section">
+      <ShowcaseHeader title="Profile" />
+      <section className="work-experience">
+        <Profile />
+        <Experiences />
+      </section>
     </section>
   )
 }
