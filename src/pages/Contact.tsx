eca0942d5926b8b079eec0b71903e707fbc8f86d
Makene Neto <makeneto17@gmail.com>
2026-05-30 18:56:27 +0100

    style: customize page title with useTitle hook

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,8 @@
 import ContactSection from "../components/contact/ContactSection"
+import { useTitle } from "../hooks/useTitle"
 
 export default function Contact() {
+  useTitle("Contact Me")
+
   return <ContactSection className="mt-0" />
 }
