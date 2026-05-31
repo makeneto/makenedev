3b4871e3ef49fb4b9acd890f99ddbf65f917732e
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:50:21 +0100

    style: apply media query in footer links

--- src/components/footer/FooterLinks.tsx
+++ src/components/footer/FooterLinks.tsx
@@ -5,8 +5,10 @@ export default function FooterLinks() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <section className="footer__links flex items-center justify-between">
-      <p className="text-gray-500">@ Makenedev | {currentYear}</p>
+    <section className="footer__links grid gap-4 sm:flex items-center justify-between">
+      <p className="text-gray-500 text-center sm:text-start">
+        @ Makenedev | {currentYear}
+      </p>
       <NavRoutes links={footerLinks} />
     </section>
   )
