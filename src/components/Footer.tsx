6694ee8052274a73c0403215f01b4e790f4a60fd
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:48:03 +0100

    style: apply media query in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Signature from "./Signature"
 export default function Footer() {
   return (
     <footer className="footer">
-      <section className="mb-20 flex items-center justify-between">
+      <section className="mb-20 grid gap-8 justify-center sm:flex sm:items-center sm:justify-between">
         <Gratitude />
         <Signature />
       </section>
