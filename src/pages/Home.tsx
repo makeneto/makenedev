216550ee8e2f7e2ed482a59cfc93fbb073c4f76a
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:46:56 +0100

    chore: rename QuoteOfTheDay to QuoteOfTheWeek

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import React from "react"
 import ContactSection from "../components/contact/ContactSection"
 import HomeHeader from "../components/headers/HomeHeader"
-import QuoteOfTheDay from "../components/QuoteOfTheDay"
 import ShowcaseSection from "../components/showcase-section/ShowcaseSection"
 import { works } from "../data/works"
+import QuoteOfTheWeek from "../components/QuoteOfTheWeek"
 
 export default function Home() {
   return (
     <React.Fragment>
       <HomeHeader />
-      <QuoteOfTheDay />
+      <QuoteOfTheWeek />
       <ShowcaseSection
         title="Last Projects"
         viewAllHref="/work"
@@ -17,7 +17,7 @@ export default function Home() {
         isHomePage
       />
 
-      <ContactSection className="mt-80" />
+      <ContactSection className="mt-60 sm:mt-80" />
     </React.Fragment>
   )
 }
