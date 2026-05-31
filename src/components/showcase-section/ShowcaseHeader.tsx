76fefbeca93d5e9dbfbe16da558055fc3cac7c10
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:52:35 +0100

    refactor: remove view all link in showcase header

--- src/components/showcase-section/ShowcaseHeader.tsx
+++ src/components/showcase-section/ShowcaseHeader.tsx
@@ -1,18 +1,11 @@
 interface ShowcaseHeaderProps {
   title: string
-  viewAllHref?: string
 }
 
-const ShowcaseHeader = ({ title, viewAllHref }: ShowcaseHeaderProps) => {
+const ShowcaseHeader = ({ title }: ShowcaseHeaderProps) => {
   return (
     <header className="showcase-header">
       <h2 className="showcase-heading">{title}</h2>
-
-      {viewAllHref && (
-        <a href={viewAllHref} className="viewAll">
-          View all
-        </a>
-      )}
     </header>
   )
 }
