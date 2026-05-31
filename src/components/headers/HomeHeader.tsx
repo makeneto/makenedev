995ff94fb5f5be72663d092b1b481be175f27893
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:51:01 +0100

    style: apply media query in gratitude section

--- src/components/headers/HomeHeader.tsx
+++ src/components/headers/HomeHeader.tsx
@@ -1,31 +1,38 @@
+import { Link } from "react-router-dom"
+import { useMediaQuery } from "react-responsive"
+
 export default function HomeHeader() {
-    return (
-        <header className="homeHeader">
-            <section className="homeHeader__heroContent">
-                <h1>Hey, </h1>
-                <img src="/assets/hero-me.png" alt="Hero image" />
-                <h1>there!</h1>
-            </section>
+  const isMobile = useMediaQuery({ maxWidth: 649 })
 
-            <article className="homeHeader__openToWork">
-                <div>
-                    <span className="homeHeader__openToWork--dot">
-                        <span className="homeHeader__openToWork--dot--inner"></span>
-                    </span>
+  return (
+    <header className="homeHeader">
+      <section className="homeHeader__heroContent">
+        <h1>Hey, </h1>
+        <img src="/assets/hero-me.png" alt="Hero image" />
+        <h1>there!</h1>
+      </section>
 
-                    <p>Available for new opportunities</p>
-                </div>
-            </article>
+      <Link to="/contact" prefetch="intent" className="homeHeader__openToWork">
+        <div>
+          <span className="homeHeader__openToWork--dot">
+            <span className="homeHeader__openToWork--dot--inner"></span>
+          </span>
 
-            <article className="homeHeader__intro">
-                <h2>
-                    I am
-                    <br />
-                    Makene
-                </h2>
+          <p>Available {!isMobile && "for new opportunities"} </p>
+        </div>
+      </Link>
 
-                <h3>Software <br /> Engineer</h3>
-            </article>
-        </header>
-    )
+      <article className="homeHeader__intro">
+        <h2>
+          I am
+          <br />
+          Makene
+        </h2>
+
+        <h3>
+          Software <br /> Engineer
+        </h3>
+      </article>
+    </header>
+  )
 }
