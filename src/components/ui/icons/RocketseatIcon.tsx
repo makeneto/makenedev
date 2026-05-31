1609f979c347a828a8ccb2e4761decb34bf2ec45
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:56:23 +0100

    style: add rocketseat icon

---
+++ src/components/ui/icons/RocketseatIcon.tsx
@@ -0,0 +1,12 @@
+import StackCard from "../../stack/StackCard"
+
+export default function RocketseatIcon() {
+  return (
+    <StackCard name="Rocketseat">
+      <img
+        src="https://i.postimg.cc/rF0WQLbj/rocketseat.webp"
+        alt="Rocketseat Logo"
+      />
+    </StackCard>
+  )
+}
