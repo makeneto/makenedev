701224bc4ff35f5b38f2a9b285c487840d3a65ed
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:50:43 +0100

    style: apply media query in gratitude section

--- src/components/footer/Gratitude.tsx
+++ src/components/footer/Gratitude.tsx
@@ -2,10 +2,10 @@ import Logo from "../Logo"
 
 export default function Gratitude() {
   return (
-    <div className="grid gap-4">
-      <Logo />
+    <div className="grid justify-center gap-4 sm:justify-start">
+      <Logo variant="footer" />
 
-      <p className="footer__gratitude">
+      <p className="footer__gratitude text-center sm:text-left">
         <span>Glade to share</span> my work!
       </p>
     </div>
