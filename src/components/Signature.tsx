dd430ae4d25b14ac50c553984a74e6620d3091bb
Makene Neto <makeneto17@gmail.com>
2026-05-31 21:49:56 +0100

    style: apply media query in signature

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -3,7 +3,7 @@ export default function Signature() {
     <img
       src="/assets/makene-white-signature.png"
       alt="Makenedev's signature"
-      className="w-32 h-auto"
+      className="w-20 h-auto sm:w-32 justify-self-center sm:justify-self-end"
     />
   )
 }
