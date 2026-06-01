e0152247d5e549d2016d09d45276f2c77550b7e6
Makene Neto <makeneto17@gmail.com>
2026-06-01 08:45:22 +0100

    style(resume frame): reduce margin top in smartphones

--- src/components/ResumeFrame.tsx
+++ src/components/ResumeFrame.tsx
@@ -6,7 +6,7 @@ export default function ResumeFrame({
   children?: React.ReactNode
 }) {
   return (
-    <section className="mt-40 grid gap-8">
+    <section className="mt-35 sm:mt-40 grid gap-8">
       <div className="w-[50%] justify-self-end border-b border-zinc-800" />
       <div className="grid gap-4">{children}</div>
       <div className="w-[50%] border-b border-zinc-800" />
