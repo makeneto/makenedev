9433b650150796a843b8ef47090de9572a8c2630
Makene Neto <makeneto17@gmail.com>
2026-06-01 08:56:06 +0100

    style(signature): center makenedev's signature and reduce your size

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -3,7 +3,7 @@ export default function Signature() {
     <img
       src="/assets/makene-white-signature.png"
       alt="Makenedev's signature"
-      className="w-20 h-auto sm:w-32 justify-self-center sm:justify-self-end"
+      className="w-18 h-auto m-auto sm:w-32 sm:m-0"
     />
   )
 }
