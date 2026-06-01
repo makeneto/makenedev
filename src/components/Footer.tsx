4c0808925ec87c89eed822f3f68e6f47ed278a34
Makene Neto <makeneto17@gmail.com>
2026-06-01 09:12:02 +0100

    style(footer): reduce margin top in smartphones

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Signature from "./Signature"
 export default function Footer() {
   return (
     <footer className="footer">
-      <section className="mb-20 grid gap-8 justify-center sm:flex sm:items-center sm:justify-between">
+      <section className="mb-15 sm:mb-20 grid gap-10 justify-center sm:flex sm:items-center sm:justify-between sm:gap-0">
         <Gratitude />
         <Signature />
       </section>
