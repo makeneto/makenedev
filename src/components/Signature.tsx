6581e41c5be48d90c404cc0a29d01d3c4ee2cfd3
Makene Neto <makeneto17@gmail.com>
2026-06-01 09:13:36 +0100

    style: increase makenedev's signature

--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -3,7 +3,7 @@ export default function Signature() {
     <img
       src="/assets/makene-white-signature.png"
       alt="Makenedev's signature"
-      className="w-18 h-auto m-auto sm:w-32 sm:m-0"
+      className="w-20 h-auto m-auto sm:w-32 sm:m-0"
     />
   )
 }
