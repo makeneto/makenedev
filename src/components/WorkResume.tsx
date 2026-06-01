ec863b7d1e963b9339f13b2aa3c81e08f65adbcb
Makene Neto <makeneto17@gmail.com>
2026-06-01 13:45:16 +0100

    style: resume text

--- src/components/WorkResume.tsx
+++ src/components/WorkResume.tsx
@@ -5,13 +5,6 @@ export default function WorkResume() {
   return (
     <ResumeFrame>
       <p className="description--medium">
-        Whether it’s building interfaces, experimenting with ideas, or crafting
-        smooth user experiences, I’m always focused on creating things that feel
-        intentional and genuinely enjoyable to use. I like blending creativity
-        with engineering to turn concepts into something real and memorable.
-      </p>
-
-      <p className="description--medium">
         If you’d like to collaborate, build something together, or simply
         connect, feel free to{" "}
         <Link to="/contact" className="hover:text-white underline">
