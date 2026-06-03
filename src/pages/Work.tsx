8cc55ce4421b011008b72310b3c24c7de4b3f696
Makene Neto <makeneto17@gmail.com>
2026-06-03 12:57:49 +0100

    fix(Work): fix title from "My Work" to "My Works"

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -12,7 +12,7 @@ export default function Work() {
   return (
     <React.Fragment>
       <DefaultHeader
-        title="My Work"
+        title="My Works"
         description="Some things you see and move on. Others stop you. Make you look twice. Make you wonder how they were made. That's the only kind of work that's worthwhile. Because attention isn't cheap everything competes for it at the same time. If your work doesn't create a pause, even for a second, then it's just noise with better typography."
       />
 
