70401c7e7aebba74418990b85fd3390637902361
Makene Neto <makeneto17@gmail.com>
2026-06-04 13:30:35 +0100

    refactor(socialLinks): add Dribbble link to social links

--- src/data/links.ts
+++ src/data/links.ts
@@ -49,6 +49,10 @@ export const socialLinks = [
     label: "Instagram",
     link: "https://instagram.com/makeneto",
   },
+  {
+    label: "Dribbble",
+    link: "https://dribbble.com/makeneto",
+  },
 ]
 
 export const contacts = [
