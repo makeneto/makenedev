3f43e2a318d12baf17d22fa3f632695843515d21
Makene Neto <makeneto17@gmail.com>
2026-06-04 20:53:49 +0100

    feat(ProfileLinks): add ProfileLinks component for displaying contact links

---
+++ src/components/about/ProfileLinks.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+import { contacts } from "../../data/links"
+
+export default function ProfileLinks() {
+  return (
+    <section className="profile__contact">
+      {contacts.map(({ label, href, display }) => (
+        <div key={label} className="flex items-center justify-between">
+          <p className="font-medium">{label}</p>
+
+          <Link
+            to={href}
+            target={label === "Email" ? "_self" : "_blank"}
+            className="text-end text-zinc-400"
+          >
+            {display}
+          </Link>
+        </div>
+      ))}
+    </section>
+  )
+}
