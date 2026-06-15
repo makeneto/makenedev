ec9b3f2735cfb44f4d5bde7663f6f7d27e138098
Makene Neto <makeneto17@gmail.com>
2026-06-15 10:32:17 +0100

    fix(signature): remove Signature component and its dependencies

--- src/components/Signature.tsx
+++
@@ -1,9 +0,0 @@
-export default function Signature() {
-  return (
-    <img
-      src="/assets/makene-white-signature.png"
-      alt="Makenedev's signature"
-      className="w-20 h-auto m-auto sm:w-32 sm:m-0"
-    />
-  )
-}
