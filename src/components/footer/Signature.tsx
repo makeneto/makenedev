31a20f44a611aebb680ba41ec198b8e97b1b9af8
Makene Neto <makeneto17@gmail.com>
2026-06-15 10:32:57 +0100

    feat(signature): add Signature component to display Makenedev's signature

---
+++ src/components/footer/Signature.tsx
@@ -0,0 +1,9 @@
+export default function Signature() {
+  return (
+    <img
+      src="/assets/makene-white-signature.png"
+      alt="Makenedev's signature"
+      className="w-20 h-auto m-auto sm:w-32 sm:m-0"
+    />
+  )
+}
