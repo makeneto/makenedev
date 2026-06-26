3dabe564812ef105076bbb83002b961ca6c9cfcf
Makene Neto <makeneto17@gmail.com>
2026-06-26 22:34:40 +0100

    feat(Blogs): add initial blog data

---
+++ src/data/blogs.ts
@@ -0,0 +1,30 @@
+export const blogs = [
+  {
+    cover: "/assets/blog-asset.jpg",
+    title: "Building in Public, and the Fear of Being Seen",
+    description:
+      "Front-end is where I started and still feel at home, but curiosity expanded my stack over time.",
+    date: "Jul 2026",
+    link: "/blogs",
+    author: "Makene Neto",
+    category: "Development",
+  },
+  {
+    cover: "/assets/blog-asset.jpg",
+    title: "AI Agents Won’t Replace You. But They Will Expose You.",
+    description: "AI Agents Won’t Replace You. But They Will Expose You.",
+    date: "Apr 2026",
+    link: "/",
+    author: "Makene Neto",
+    category: "AI",
+  },
+  {
+    cover: "/assets/blog-asset.jpg",
+    title: "AI Agents Won’t Replace You. But They Will Expose You.",
+    description: "AI Agents Won’t Replace You. But They Will Expose You.",
+    date: "Jan 2026",
+    link: "/stack",
+    author: "Makene Neto",
+    category: "AI",
+  },
+]
