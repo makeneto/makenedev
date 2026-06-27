4e80d0ecf4e4770519d001d336bce47cf6603f76
Makene Neto <makeneto17@gmail.com>
2026-06-27 22:05:16 +0100

    feat(BlogList): refactor blog list to use BlogsGrid component and improve layout

--- src/components/blogs/BlogList.tsx
+++ src/components/blogs/BlogList.tsx
@@ -1,39 +1,17 @@
-import { Link } from "react-router-dom"
 import { blogs } from "../../data/blogs"
-import { ArrowUpRight } from "lucide-react"
+import { ViewAllLink } from "../showcase-section/ViewAllLink"
+import BlogsGrid from "./BlogsGrid"
+import type { Blog } from "../../interfaces/blog"
 
 export default function BlogList() {
+  const firstBlogs: Blog[] = blogs.slice(0, 5)
+  const hasMoreBlogs = blogs.length > 5
+
   return (
-    <ul className="blog-list">
-      {blogs.map((blog) => (
-        <li key={`${blog.link}-${blog.title}`} className="blog-list__item">
-          <Link to={blog.link} className="blog-card">
-            <div className="blog-card__media">
-              <img
-                src={blog.cover}
-                alt={blog.title}
-                className="blog-card__cover"
-              />
-              <div className="blog-card__overlay">
-                <span className="blog-card__category">{blog.category}</span>
-                <span className="blog-card__date">{blog.date}</span>
-                <span className="blog-card__author blog-card__author--overlay">
-                  By {blog.author}
-                </span>
-              </div>
-            </div>
-            <div className="blog-card__content">
-              <h3 className="blog-card__title">{blog.title}</h3>
-              <p className="blog-card__description">{blog.description}</p>
-              <div className="blog-card__footer">
-                <span className="blog-card__link">
-                  Read more <ArrowUpRight />
-                </span>
-              </div>
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <section className="blog-list-section">
+      <BlogsGrid items={firstBlogs} />
+
+      {hasMoreBlogs && <ViewAllLink to="/blogs" />}
+    </section>
   )
 }
