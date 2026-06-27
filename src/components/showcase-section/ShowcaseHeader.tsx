d554ca6e3a2d7e91593cfbf87f8e811568c28251
Makene Neto <makeneto17@gmail.com>
2026-06-27 22:07:53 +0100

    feat(ShowcaseHeader): add viewAll prop to conditionally display header text and link

--- src/components/showcase-section/ShowcaseHeader.tsx
+++ src/components/showcase-section/ShowcaseHeader.tsx
@@ -1,11 +1,18 @@
+import { ViewAllLink } from "./ViewAllLink"
+
 interface ShowcaseHeaderProps {
   title: string
+  viewAll?: string
 }
 
-const ShowcaseHeader = ({ title }: ShowcaseHeaderProps) => {
+const ShowcaseHeader = ({ title, viewAll = "" }: ShowcaseHeaderProps) => {
   return (
     <header className="showcase-header">
-      <h2 className="showcase-heading">{title}</h2>
+      <h2 className="showcase-heading">
+        {viewAll ? "Last" : "All"} {title}
+      </h2>
+
+      {viewAll && <ViewAllLink to={viewAll} />}
     </header>
   )
 }
