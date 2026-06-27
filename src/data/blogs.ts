daa15ceb96b6cd022f73e0372d175a0ade5f2308
Makene Neto <makeneto17@gmail.com>
2026-06-27 22:10:40 +0100

    feat(blogs): add new posts and update links and categories

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -1,6 +1,20 @@
-export const blogs = [
+import type { Blog } from "../interfaces/blog"
+
+export const blogs: Blog[] = [
   {
-    cover: "/assets/blog-asset.jpg",
+    cover:
+      "https://images.unsplash.com/photo-1781989302295-ce96b5be39d8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Building in Public, and the Fear of Being Seen",
+    description:
+      "Front-end is where I started and still feel at home, but curiosity expanded my stack over time.",
+    date: "Jul 2026",
+    link: "/contact",
+    author: "Makene Neto",
+    category: "Development",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1781989302295-ce96b5be39d8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Building in Public, and the Fear of Being Seen",
     description:
       "Front-end is where I started and still feel at home, but curiosity expanded my stack over time.",
@@ -24,6 +38,26 @@ export const blogs = [
     description: "AI Agents Won’t Replace You. But They Will Expose You.",
     date: "Jan 2026",
     link: "/stack",
+    author: "Makene Neto",
+    category: "AI",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1781989302295-ce96b5be39d8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Building in Public, and the Fear of Being Seen",
+    description:
+      "Front-end is where I started and still feel at home, but curiosity expanded my stack over time.",
+    date: "Jul 2026",
+    link: "/about",
+    author: "Makene Neto",
+    category: "Development",
+  },
+  {
+    cover: "/assets/blog-asset.jpg",
+    title: "AI Agents Won’t Replace You. But They Will Expose You.",
+    description: "AI Agents Won’t Replace You. But They Will Expose You.",
+    date: "Apr 2026",
+    link: "/blogs",
     author: "Makene Neto",
     category: "AI",
   },
