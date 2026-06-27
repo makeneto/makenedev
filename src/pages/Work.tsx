466164f7e528d23c85a9dd784a960a33f82cee6f
Makene Neto <makeneto17@gmail.com>
2026-06-27 22:13:15 +0100

    fix(work): update the Showcase section title to "Projects"

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -14,9 +14,10 @@ export default function Work() {
       <DefaultHeader
         title="My Works"
         description="Some things you see and move on. Others stop you. Make you look twice. Make you wonder how they were made. That's the only kind of work that's worthwhile. Because attention isn't cheap everything competes for it at the same time. If your work doesn't create a pause, even for a second, then it's just noise with better typography."
+        isShowcase={true}
       />
 
-      <ShowcaseSection title="All Works" items={works} />
+      <ShowcaseSection title="Projects" items={works} />
       <WorkResume />
     </React.Fragment>
   )
