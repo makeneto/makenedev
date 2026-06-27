10d165e20b420abc3ca2d10e201f07efc307f1fd
Makene Neto <makeneto17@gmail.com>
2026-06-27 22:07:18 +0100

    feat(DefaultHeader): add isShowcase prop and adjust header margin styles

--- src/components/headers/DefaultHeader.tsx
+++ src/components/headers/DefaultHeader.tsx
@@ -2,19 +2,21 @@ interface DefaultHeaderProps {
   title: string
   description?: string
   component?: React.ReactNode
+  isShowcase?: boolean
 }
 
 export default function DefaultHeader({
   title,
   description,
   component,
+  isShowcase = false,
 }: DefaultHeaderProps) {
   return (
-    <header className="default-header">
+    <header
+      className={`default-header ${!isShowcase ? "mb-28 sm:mb-40" : "mb-14 sm:mb-20"}`}
+    >
       <h1 className="default-header__title">{title}</h1>
-      {description && (
-        <p className="description--normal">{description}</p>
-      )}
+      {description && <p className="description--normal">{description}</p>}
       {component && <div>{component}</div>}
     </header>
   )
