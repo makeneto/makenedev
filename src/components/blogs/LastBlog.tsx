d622e0ef1a37674dfbaf48ff8207d3b447b840fc
Makene Neto <makeneto17@gmail.com>
2026-06-28 19:41:18 +0100

    feat(blog): develop last blog card

---
+++ src/components/blogs/LastBlog.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import { blogs } from "../../data/blogs"
+
+export default function LastBlog() {
+  const lastBlog = blogs[0]
+  const { link, category, date, title, description, author, cover } = lastBlog
+
+  return (
+    <Link to={link} className="blog-lastBlog">
+      <div>
+        <span>{category}</span>
+        <h1>{title}</h1>
+        <p>{description}</p>
+
+        <div className="flex items-center justify-between">
+          <span>By {author}</span>
+          <span>{date}</span>
+        </div>
+      </div>
+
+      <img src={cover} alt={title + `Cover`} />
+    </Link>
+  )
+}
