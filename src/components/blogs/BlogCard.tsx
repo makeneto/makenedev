e5ebd57e569446f515787ed72c9a8dc162cf7d81
Makene Neto <makeneto17@gmail.com>
2026-06-28 19:39:11 +0100

    feat(blog): develop blog cad

---
+++ src/components/blogs/BlogCard.tsx
@@ -0,0 +1,34 @@
+import { ArrowUpRight } from "lucide-react"
+import { Link } from "react-router-dom"
+import type { Blog } from "../../interfaces/blog"
+
+export default function BlogCard({ blog }: { blog: Blog }) {
+  const { link, cover, title, author, date, category, description } = blog
+
+  return (
+    <Link to={link} className="blog-card">
+      <div className="blog-card__media">
+        <img src={cover} alt={title} className="blog-card__cover" />
+        <div className="blog-card__overlay">
+          <div className="grid">
+            <span className="blog-card__author blog-card__author--overlay">
+              {author}
+            </span>
+            <span className="blog-card__date">{date}</span>
+          </div>
+
+          <span className="blog-card__category">{category}</span>
+        </div>
+      </div>
+      <div className="blog-card__content">
+        <h3 className="blog-card__title">{title}</h3>
+        <p className="blog-card__description">{description}</p>
+        <div className="blog-card__footer">
+          <span className="blog-card__link">
+            Read more <ArrowUpRight />
+          </span>
+        </div>
+      </div>
+    </Link>
+  )
+}
