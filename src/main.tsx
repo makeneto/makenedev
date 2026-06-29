e8e37df9bb5c6eb2fdfb1a91920b00a443da655e
Makene Neto <makeneto17@gmail.com>
2026-06-29 19:57:15 +0100

    feat(app): setup React Query provider with QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,30 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import App from "./App.tsx"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import "./css/style.css"
 import "./css/index.css"
 
+import App from "./App.tsx"
 import { Toaster } from "../src/components/ui/sonner.tsx"
 import Navbar from "./components/navbar/Navbar.tsx"
 import Footer from "./components/footer/Footer.tsx"
 import ScrollToTop from "./hooks/scrollToTop.ts"
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ScrollToTop />
-      <Navbar />
-      <App />
-      <Footer />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Navbar />
+        <App />
+        <Footer />
+      </BrowserRouter>
 
-    <Toaster />
+      <Toaster />
+    </QueryClientProvider>
   </StrictMode>,
 )
