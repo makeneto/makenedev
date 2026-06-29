e27ce9654f7fe507a15fd30a603b02a06c10657b
Makene Neto <makeneto17@gmail.com>
2026-06-29 19:52:42 +0100

    style(quote): set default quote

--- src/components/QuoteOfTheWeek.tsx
+++ src/components/QuoteOfTheWeek.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+import { fallbackQuote } from "../data/fallbackQuote"
 import { useQuote } from "../hooks/useQuote"
 import { weekCounter } from "../utils/weekCounter"
-import { Link } from "react-router-dom"
 
 export default function QuoteOfTheWeek() {
   const { currentWeek, totalWeeks } = weekCounter()
-  const {loading, data} = useQuote()
-  
+  const { data, isLoading } = useQuote()
+
+  const quote = data?.quote ?? fallbackQuote
 
   return (
     <section className="quoteOfTheWeek">
@@ -20,15 +22,16 @@ export default function QuoteOfTheWeek() {
 
       <div className="quoteOfTheWeek__content">
         <h1>
-          “<span>{loading ? "Loading..." : data?.quote?.text}</span>”
+          “<span>{isLoading ? "Loading..." : quote.text}</span>”
         </h1>
 
         <div className="flex items-center justify-between">
-          <small translate="no">— {loading ? "Loading..." : data?.quote?.author}</small>
+          <small translate="no">
+            — {isLoading ? "Loading..." : quote.author}
+          </small>
 
           <Link
             to="https://makenedev-primeapi.vercel.app/quote/week"
-            prefetch="intent"
             target="_blank"
             className="text-xs text-zinc-700 hover:text-white hover:underline"
           >
