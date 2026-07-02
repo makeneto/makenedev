78cb8acf8c51ec625960164883397c5bb8b9b166
Makene Neto <makeneto17@gmail.com>
2026-07-02 21:36:03 +0100

    feat(blog): wrap LastBlog component in a section for improved structure

--- src/components/blogs/LastBlog.tsx
+++ src/components/blogs/LastBlog.tsx
@@ -6,19 +6,19 @@ export default function LastBlog() {
   const { link, category, date, title, description, author, cover } = lastBlog
 
   return (
-    <Link to={link} className="blog-lastBlog">
-      <div>
-        <span>{category}</span>
-        <h1>{title}</h1>
-        <p>{description}</p>
-
-        <div className="flex items-center justify-between">
-          <span>By {author}</span>
-          <span>{date}</span>
+    <section className="blog-lastBlog">
+      <Link to={link} className="blog-lastBlog-card">
+        <div>
+          <span>{category}</span>
+          <h1>{title}</h1>
+          <p>{description}</p>
+          <div className="flex items-center justify-between">
+            <span>By {author}</span>
+            <span>{date}</span>
+          </div>
         </div>
-      </div>
-
-      <img src={cover} alt={title + `Cover`} />
-    </Link>
+        <img src={cover} alt={title + `Cover`} />
+      </Link>
+    </section>
   )
 }
