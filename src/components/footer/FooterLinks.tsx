d302835d8d8492120b5a2194f5b79a86fc95ef98
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:45:51 +0100

    fix(FooterLinks): update text color for better contrast and adjust border styling

--- src/components/footer/FooterLinks.tsx
+++ src/components/footer/FooterLinks.tsx
@@ -5,8 +5,8 @@ export default function FooterLinks() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <section className="w-screen sm:w-auto grid justify-center gap-4 sm:flex items-center sm:justify-between border-t border-zinc-800 pt-6">
-      <p className="text-gray-500 text-sm text-center sm:text-start">
+    <section className="w-screen sm:w-auto grid justify-center gap-4 sm:flex items-center sm:justify-between border-t border-[--border-primary] pt-6">
+      <p className="text-zinc-500 dark:text-zinc-400 text-sm text-center sm:text-start">
         @ Makene Neto | {currentYear}
       </p>
 
