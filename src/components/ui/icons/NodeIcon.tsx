9fc2d2f80f7c15f353932771b037f5793e3555cf
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:47:52 +0100

    fix(NodeIcon): update mask fill attributes to use Tailwind CSS classes for consistency

--- src/components/ui/icons/NodeIcon.tsx
+++ src/components/ui/icons/NodeIcon.tsx
@@ -55,7 +55,7 @@ export default function NodeIcon() {
           fill="url(#nodejs__a)"
           d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944L134.923 1.832Z"
         />
-        <mask id="nodejs__d" fill="#fff">
+        <mask id="nodejs__d" className="fill-black dark:fill-white">
           <use xlinkHref="#nodejs__b" />
         </mask>
         <path
@@ -63,7 +63,7 @@ export default function NodeIcon() {
           d="M249.485 67.8 134.65 1.833c-1.086-.542-2.443-1.085-3.529-1.357L2.443 220.912c1.086 1.357 2.444 2.443 3.8 3.258l114.834 65.968c3.258 1.9 7.059 2.443 10.588 1.357L252.47 70.515c-.815-1.086-1.9-1.9-2.986-2.714Z"
           mask="url(#nodejs__d)"
         />
-        <mask id="nodejs__g" fill="#fff">
+        <mask id="nodejs__g" className="fill-black dark:fill-white">
           <use xlinkHref="#nodejs__e" />
         </mask>
         <path
