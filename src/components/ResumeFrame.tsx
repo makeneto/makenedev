94961b27a873671a17fd4d5aa68e4ef90bec1a0e
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:43:14 +0100

    fix(ResumeFrame): update border color to use primary theme variable

--- src/components/ResumeFrame.tsx
+++ src/components/ResumeFrame.tsx
@@ -7,9 +7,9 @@ export default function ResumeFrame({
 }) {
   return (
     <section className="mt-35 sm:mt-40 grid gap-8">
-      <div className="w-[50%] justify-self-end border-b border-zinc-800" />
+      <div className="w-[50%] justify-self-end border-b border-[--border-primary]" />
       <div className="grid gap-4">{children}</div>
-      <div className="w-[50%] border-b border-zinc-800" />
+      <div className="w-[50%] border-b border-[--border-primary]" />
     </section>
   )
 }
