a1bb37dd2e0a44558337ee4d1ac9ee8d0ca07fbd
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:46:10 +0100

    fix(Signature): update signature image source based on theme

--- src/components/footer/Signature.tsx
+++ src/components/footer/Signature.tsx
@@ -1,7 +1,16 @@
+import { useTheme } from "next-themes"
+
 export default function Signature() {
+  const { resolvedTheme } = useTheme()
+  const isLightTheme = resolvedTheme === "light"
+
   return (
     <img
-      src="/assets/makene-white-signature.png"
+      src={
+        isLightTheme
+          ? "/assets/makenedev-light-signature.png"
+          : "/assets/makenedev-dark-signature.png"
+      }
       alt="Makenedev's signature"
       className="w-20 h-auto m-auto sm:w-32 sm:m-0"
     />
