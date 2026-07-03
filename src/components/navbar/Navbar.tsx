2f322819d5d5c0ef224476d4427d32837caf7cca
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:46:17 +0100

    fix(Navbar): improve layout and responsiveness by adjusting theme colors and adding mobile support

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,17 +2,30 @@ import { navLinks } from "../../data/links"
 import Logo from "../Logo"
 import SocialLinks from "../SocialLinks"
 import NavRoutes from "./NavRoutes"
+import ThemeToggle from "../ThemeToggle"
+import { useMediaQuery } from "react-responsive"
 
 export default function Navbar() {
+  const isMobile = useMediaQuery({ maxWidth: 768 })
+
   return (
     <nav className="nav flex items-center justify-between">
       <div className="flex items-center gap-3 sm:gap-5">
         <Logo variant="nav" />
-        <div className="w-px h-7 bg-zinc-800" />
+        <div className="w-px h-7 bg-zinc-200 dark:bg-zinc-800" />
         <NavRoutes links={navLinks} />
       </div>
 
-      <SocialLinks />
+      <div className="flex items-center sm:gap-4">
+        <SocialLinks />
+
+        {!isMobile && (
+          <>
+            <div className="w-px h-5 bg-zinc-200 dark:bg-zinc-800" />
+            <ThemeToggle />
+          </>
+        )}
+      </div>
     </nav>
   )
 }
