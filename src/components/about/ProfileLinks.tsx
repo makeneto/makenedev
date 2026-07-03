11e47e6388f4bdbecd9bd32c904c8d0c135ff8ba
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:44:43 +0100

    fix(ProfileLinks): update link text color for better visibility

--- src/components/about/ProfileLinks.tsx
+++ src/components/about/ProfileLinks.tsx
@@ -12,7 +12,9 @@ export default function ProfileLinks() {
           <Link
             to={href}
             target={label === "Email" ? "_self" : "_blank"}
-            className="sm:text-sm text-end text-zinc-400"
+            className="sm:text-sm text-end 
+            text-zinc-500
+            dark:text-zinc-400"
           >
             {display}
           </Link>
