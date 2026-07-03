54b047958969c5465d37a355a4186b204a67df0b
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:43:25 +0100

    feat(ThemeProvider): add ThemeProvider component for theming support

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+interface ThemeProviderProps {
+  children: ReactNode
+}
+
+export default function ThemeProvider({ children }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem={true}
+      enableColorScheme={true}
+      disableTransitionOnChange={true}
+      storageKey="theme"
+      forcedTheme={undefined}
+    >
+      {children}
+    </NextThemesProvider>
+  )
+}
