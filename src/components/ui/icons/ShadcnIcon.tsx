15a42b74812c7d58e710cdbf35d68710f86f79af
Makene Neto <makeneto17@gmail.com>
2026-07-03 20:48:15 +0100

    fix(ShadcnIcon): update stroke to use Tailwind CSS classes for consistency

--- src/components/ui/icons/ShadcnIcon.tsx
+++ src/components/ui/icons/ShadcnIcon.tsx
@@ -7,7 +7,7 @@ export default function ShadcnIcon() {
         <path fill="none" d="M0 0h256v256H0z" />
         <path
           fill="none"
-          stroke="#fff"
+          className="stroke-black dark:stroke-white"
           strokeWidth="25"
           strokeLinecap="round"
           d="M208 128l-80 80M192 40L40 192"
