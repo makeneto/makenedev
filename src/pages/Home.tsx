bdc3ec1ee5049fe993326c13e5d06a0921d2ce74
Makene Neto <makeneto17@gmail.com>
2026-07-04 18:04:38 +0100

    style(Home): uncomment VerticalBlogList component for display on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 
+import { works } from "../data/works"
 import ContactSection from "../components/contact/ContactSection"
 import HomeHeader from "../components/headers/HomeHeader"
 import ShowcaseSection from "../components/showcase-section/ShowcaseSection"
-import { works } from "../data/works"
 import QuoteOfTheWeek from "../components/QuoteOfTheWeek"
+import VerticalBlogList from "../components/blogs/VerticalBlogList"
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
       <QuoteOfTheWeek />
       <ShowcaseSection title="Works" viewAll="/work" items={works} isHomePage />
 
-      {/* <VerticalBlogList /> */}
+      <VerticalBlogList />
       <ContactSection className="mt-50 sm:mt-70" />
     </React.Fragment>
   )
