2998c7e970f93f82ed7a0e5bad312786b9288f91
Makene Neto <makeneto17@gmail.com>
2026-07-05 15:49:49 +0100

    feat(ViewAllLink): add customizable textLink prop for dynamic link text

--- src/components/showcase-section/ViewAllLink.tsx
+++ src/components/showcase-section/ViewAllLink.tsx
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom"
 
 interface ViewAllLinkProps {
   to: string
+  textLink?: string
 }
 
-export const ViewAllLink: React.FC<ViewAllLinkProps> = ({ to }) => (
+export const ViewAllLink: React.FC<ViewAllLinkProps> = ({
+  to,
+  textLink = "View all",
+}) => (
   <Link
     to={to}
     prefetch="intent"
-    className="flex items-center gap-2 text-sm text-zinc-400 transition-all hover:text-black dark:hover:text-white hover:gap-1"
+    className="flex items-center gap-2 text-sm text-zinc-600 transition-all hover:text-black dark:hover:text-white hover:gap-1"
   >
-    View all
+    {textLink}
     <ArrowRight size={16} />
   </Link>
 )
