25577b157e50760cbece0485525a4993c056acc4
Makene Neto <makeneto17@gmail.com>
2026-07-05 15:50:51 +0100

    feat(services): add service icon map with imported icons

---
+++ src/lib/servicesIconMap.ts
@@ -0,0 +1,15 @@
+import {
+  RiCodeSSlashLine,
+  RiCoinLine,
+  RiPaletteLine,
+  RiSmartphoneLine,
+  RiSparklingLine,
+} from "@remixicon/react"
+
+export const servicesIconMap = {
+  RiCodeSSlashLine: RiCodeSSlashLine,
+  RiSparklingLine: RiSparklingLine,
+  RiSmartphoneLine: RiSmartphoneLine,
+  RiPaletteLine: RiPaletteLine,
+  RiCoinLine: RiCoinLine,
+} as const
