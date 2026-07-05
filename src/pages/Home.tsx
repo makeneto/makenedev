e4b901adede58d97afad3a95e8a771878d9b401d
Makene Neto <makeneto17@gmail.com>
2026-07-05 15:48:48 +0100

    style(Home): update ShowcaseSection title for consistency and add MyServices component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,15 @@ import ContactSection from "../components/contact/ContactSection"
 import HomeHeader from "../components/headers/HomeHeader"
 import ShowcaseSection from "../components/showcase-section/ShowcaseSection"
 import QuoteOfTheWeek from "../components/QuoteOfTheWeek"
+import MyServices from "../components/myServices/MyServices"
 
 export default function Home() {
   return (
     <React.Fragment>
       <HomeHeader />
+      <MyServices />
       <QuoteOfTheWeek />
-      <ShowcaseSection title="Works" viewAll="/work" items={works} isHomePage />
+      <ShowcaseSection title="Last Works" viewAll="/work" items={works} isHomePage />
 
       {/* <VerticalBlogList /> */}
       <ContactSection className="mt-50 sm:mt-70" />
