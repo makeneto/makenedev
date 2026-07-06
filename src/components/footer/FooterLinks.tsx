0dcf0425f6835a0b56ab3a932c8b8ecb6cba1c6d
Makene Neto <makeneto17@gmail.com>
2026-07-06 13:38:32 +0100

    fix(footer-links): update copyright symbol for correct display

--- src/components/footer/FooterLinks.tsx
+++ src/components/footer/FooterLinks.tsx
@@ -7,7 +7,7 @@ export default function FooterLinks() {
   return (
     <section className="w-screen sm:w-auto grid justify-center gap-4 sm:flex items-center sm:justify-between border-t border-[--border-primary] pt-6">
       <p className="text-zinc-500 dark:text-zinc-400 text-sm text-center sm:text-start">
-        @ Makene Neto | {currentYear}
+        © Makene Neto | {currentYear}
       </p>
 
       <NavRoutes links={footerLinks} />
