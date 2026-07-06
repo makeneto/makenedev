db93c04f6f93b33e4063ef46107bc2c0bc2bf2e0
Makene Neto <makeneto17@gmail.com>
2026-07-06 13:39:10 +0100

    feat(tool-card): add ToolCard component for displaying tool information

---
+++ src/components/setup/ToolCard.tsx
@@ -0,0 +1,35 @@
+import { ExternalLink } from "lucide-react"
+import { Link } from "react-router-dom"
+
+interface ToolCardProps {
+  image: string
+  name: string
+  description: string
+  link: string
+}
+
+export default function ToolCard({
+  image,
+  name,
+  description,
+  link,
+}: ToolCardProps) {
+  return (
+    <Link
+      className="setup-card"
+      to={link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src={image} alt={name} />
+
+      <section className="setup-card__content">
+        <div>
+          <h3>{name}</h3>
+          <ExternalLink />
+        </div>
+        <p>{description}</p>
+      </section>
+    </Link>
+  )
+}
