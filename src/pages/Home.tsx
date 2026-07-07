bd4cde3615317fb177addcad1a767b4bf48dd6fc
Makene Neto <makeneto17@gmail.com>
2026-07-07 23:46:45 +0100

    feat(home): replace ContactSection with Cta component and format ShowcaseSection props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 
 import { works } from "../data/works"
-import ContactSection from "../components/contact/ContactSection"
 import HomeHeader from "../components/headers/HomeHeader"
 import ShowcaseSection from "../components/showcase-section/ShowcaseSection"
 import QuoteOfTheWeek from "../components/QuoteOfTheWeek"
 import MyServices from "../components/myServices/MyServices"
+import Cta from "../components/contact/Cta"
 
 export default function Home() {
   return (
@@ -13,10 +13,15 @@ export default function Home() {
       <HomeHeader />
       <MyServices />
       <QuoteOfTheWeek />
-      <ShowcaseSection title="Last Works" viewAll="/work" items={works} isHomePage />
+      <ShowcaseSection
+        title="Last Works"
+        viewAll="/work"
+        items={works}
+        isHomePage
+      />
 
       {/* <VerticalBlogList /> */}
-      <ContactSection className="mt-50 sm:mt-70" />
+      <Cta className="mt-50 sm:mt-70" />
     </React.Fragment>
   )
 }
