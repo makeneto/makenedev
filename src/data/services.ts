ca92d9ced66876d7ee428d2ac8c4147c991895d3
Makene Neto <makeneto17@gmail.com>
2026-07-08 14:20:00 +0100

    fix(Services): remove unnecessary service icons for simplification

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,29 +1,24 @@
 export const services = [
   {
-    icon: "RiCodeSSlashLine",
     title: "Web Development",
     description:
       "Create fast and responsive websites using modern web technologies.",
   },
   {
-    icon: "RiSparklingLine",
     title: "AI Integration",
     description: "Integrate smart AI features into your apps and workflows.",
   },
   {
-    icon: "RiSmartphoneLine",
     title: "Mobile Development",
     description:
       "Create stunning mobile applications for iOS and Android platforms.",
   },
   {
-    icon: "RiPaletteLine",
     title: "UI/UX Design",
     description:
       "Create beautiful and intuitive interfaces for web and mobile apps.",
   },
   {
-    icon: "RiCoinLine",
     title: "Gold Buying",
     description: "Buy and evaluate gold with security and full transparency.",
   },
