7a9f25090c1cbccac5473597cbab9514fae0af64
Makene Neto <makeneto17@gmail.com>
2026-07-08 14:16:36 +0100

    fix(ContactSectionHeader): update component to accept dynamic description prop

--- src/components/contact/ContactSectionHeader.tsx
+++ src/components/contact/ContactSectionHeader.tsx
@@ -1,4 +1,4 @@
-export function ContactSectionHeader() {
+export function ContactSectionHeader({ description }: { description: string }) {
   return (
     <header>
       <span className="contact__subtitle">Contact me</span>
@@ -9,11 +9,7 @@ export function ContactSectionHeader() {
         <i>Touch.</i>
       </h2>
 
-      <p className="contact__description">
-        I build scalable, high-performance digital solutions with clean,
-        maintainable architecture and a strong focus on efficiency and
-        measurable impact.
-      </p>
+      <p className="contact__description">{description}</p>
     </header>
   )
 }
