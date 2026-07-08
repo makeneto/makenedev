23883761ef9c2b6d8d1627ba59c9f877ba16bb33
Makene Neto <makeneto17@gmail.com>
2026-07-08 14:17:46 +0100

    fix(FooterLinks): replace paragraph with span for copyright text

--- src/components/footer/FooterLinks.tsx
+++ src/components/footer/FooterLinks.tsx
@@ -6,9 +6,9 @@ export default function FooterLinks() {
 
   return (
     <section className="w-screen sm:w-auto grid justify-center gap-4 sm:flex items-center sm:justify-between border-t border-[--border-primary] pt-6">
-      <p className="text-zinc-500 dark:text-zinc-400 text-sm text-center sm:text-start">
+      <span className="copyright text-sm text-center sm:text-start">
         © Makene Neto | {currentYear}
-      </p>
+      </span>
 
       <NavRoutes links={footerLinks} />
     </section>
