a1e379155f524107a409b8ceeb6bc03b036cf82e
Makene Neto <makeneto17@gmail.com>
2026-07-08 14:17:41 +0100

    fix(MyServices): adjust section margin and simplify ShowcaseHeader

--- src/components/myServices/MyServices.tsx
+++ src/components/myServices/MyServices.tsx
@@ -3,12 +3,8 @@ import ServicesList from "./ServicesList"
 
 export default function MyServices() {
   return (
-    <section className="mb-50 sm:mb-70">
-      <ShowcaseHeader
-        title="My Services"
-        linkSection="/contact"
-        textLink="Contact"
-      />
+    <section className="mb-50 sm:mb-70 xl:mb-90">
+      <ShowcaseHeader title="My Services" />
 
       <ServicesList />
     </section>
