a4aa9b2094832196212ccbf51fbe538b4961b14a
Makene Neto <makeneto17@gmail.com>
2026-07-17 14:53:59 +0100

    fix: remove isHomePage prop and simplify ShowcaseGrid component

--- src/components/showcase-section/ShowcaseGrid.tsx
+++ src/components/showcase-section/ShowcaseGrid.tsx
@@ -3,15 +3,11 @@ import ShowcaseCard from "./ShowcaseCard"
 
 interface ShowcaseGridProps {
   items: ShowcaseItem[]
-  isHomePage: boolean
 }
 
-const ShowcaseGrid = ({ items, isHomePage }: ShowcaseGridProps) => {
+const ShowcaseGrid = ({ items }: ShowcaseGridProps) => {
   return (
-    <ul
-      className={`showCaseGrid ${!isHomePage ? "showCaseGrid--multi-row" : ""}`}
-      role="list"
-    >
+    <ul className="showCaseGrid" role="list">
       {items.map((item) => (
         <li key={item.id}>
           <ShowcaseCard item={item} />
