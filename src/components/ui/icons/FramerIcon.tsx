ec66b757a3444fbf4f9d02c53c9c5b3b19b5634b
Makene Neto <makeneto17@gmail.com>
2026-07-17 12:51:42 +0100

    Feature: Add FramerIcon component

---
+++ src/components/ui/icons/FramerIcon.tsx
@@ -0,0 +1,14 @@
+import StackCard from "../../stack/StackCard"
+
+export default function FramerIcon() {
+  return (
+    <StackCard name="Framer">
+      <svg viewBox="0 0 256 384" preserveAspectRatio="xMidYMid">
+        <path
+          className="fill-black dark:fill-white" 
+          d="M0 0h256v128H128L0 0Zm0 128h128l128 128H128v128L0 256V128Z"
+        />
+      </svg>
+    </StackCard>
+  )
+}
