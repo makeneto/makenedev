da9d1afdf4371d03b8e6f608bb8fb81b49868d1b
Makene Neto <makeneto17@gmail.com>
2026-07-28 16:37:58 +0100

    refactor: update import paths to use absolute imports for better readability and maintainability

--- src/components/pricing/PricingSection.tsx
+++ src/components/pricing/PricingSection.tsx
@@ -1,11 +1,11 @@
-import { pricingPlans } from "../../data/pricing"
+import { pricingPlans } from "@/data/pricing"
 import ShowcaseHeader from "../showcase-section/ShowcaseHeader"
 import PricingCard from "./PricingCard"
 
 export default function PricingSection() {
   return (
     <section className="pricing my-50 sm:my-70 xl:my-90">
-      <ShowcaseHeader title="Pricing"/> 
+      <ShowcaseHeader title="Pricing" />
       <div className="pricing__grid">
         <PricingCard key={pricingPlans[0].id} plan={pricingPlans[0]} />
 
