c417fbc6cf18f43383db558e3ba8e1cb57e80a59
Makene Neto <makeneto17@gmail.com>
2026-08-03 13:49:40 +0100

    refactor: adjust text formatting in the BlogResume component

--- src/components/blogs/BlogResume.tsx
+++ src/components/blogs/BlogResume.tsx
@@ -4,10 +4,15 @@ export default function BlogResume() {
   return (
     <ResumeFrame>
       <p className="description--medium">
-       I don't write because I have all the answers. I write because thinking becomes clearer when ideas are turned into words.</p>
+        I don&apos;t write because I have all the answers. I write because
+        thinking becomes clearer when ideas are turned into words.
+      </p>
 
       <p className="description--medium">
-        Every article begins with curiosity. Sometimes it's a question, sometimes an observation, and sometimes an idea worth challenging. Writing helps me understand the world and sharing it is an invitation for others to think alongside me.
+        Every article begins with curiosity. Sometimes it&apos;s a question,
+        sometimes an observation, and sometimes an idea worth challenging.
+        Writing helps me understand the world and sharing it is an invitation
+        for others to think alongside me.
       </p>
     </ResumeFrame>
   )
