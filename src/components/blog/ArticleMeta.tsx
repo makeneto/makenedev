df8d4ff74a120702782d0382a847134e6fe06e97
Makene Neto <makeneto17@gmail.com>
2026-08-11 21:48:07 +0100

    chore: develop ArticleMeta component to render blog meta

---
+++ src/components/blog/ArticleMeta.tsx
@@ -0,0 +1,42 @@
+import { Clock3 } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+
+import { BlogArticle, readingTime } from "@/services/wisp"
+
+interface ArticleMetaProps {
+  post: BlogArticle
+}
+
+export function ArticleMeta({ post }: ArticleMetaProps) {
+  const { content } = post
+
+  return (
+    <React.Fragment>
+      <div>
+        <p>Author</p>
+
+        <Link href="/about" className="flex items-center gap-2">
+          <Image
+            src="https://github.com/makeneto.png"
+            alt="Makene's profile"
+            width={20}
+            height={20}
+            className="rounded-full"
+          />
+          <p>Makene Neto</p>
+        </Link>
+      </div>
+
+      <div>
+        <p>Read time</p>
+
+        <span className="flex items-center gap-2">
+          <Clock3 size={14} strokeWidth={1.8} />
+          <p>{readingTime(content)}</p>
+        </span>
+      </div>
+    </React.Fragment>
+  )
+}
