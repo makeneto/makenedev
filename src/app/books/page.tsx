a9a31ec1f97d75cc7b1f258375957f0ef716ed77
Makene Neto <makeneto17@gmail.com>
2026-08-11 21:49:30 +0100

    chore: remove unusable native link import

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Image from "next/image"
-import Link from "next/link"
 
 import DefaultHeader from "@/components/headers/DefaultHeader"
 import BookResume from "@/components/books/BookResume"
