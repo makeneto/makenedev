944ef037bed4ae3a909c6bb501205d063404205a
Makene Neto <makeneto17@gmail.com>
2026-08-11 21:49:59 +0100

    chore: develop ArticleView component to render number of views

---
+++ src/components/blog/ArticleView.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useMemo } from "react"
+import Link from "next/link"
+import { ChevronLeft } from "lucide-react"
+
+import { type BlogArticle } from "@/services/wisp"
+import { processBlogContent } from "@/utils/processBlogContent"
+import BlogIntro from "./BlogIntro"
+import BlogAside from "./BlogAside"
+import { Engagement } from "./Engagement"
+
+export function ArticleView({ post }: { post: BlogArticle }) {
+  const { html } = useMemo(
+    () => processBlogContent(post.content),
+    [post.content],
+  )
+
+  return (
+    <article>
+      <BlogIntro post={post} />
+
+      <section className="mt-20 relative grid sm:grid-cols-[70%_auto] gap-20 xl:gap-15">
+        <div className="relative">
+          <div
+            dangerouslySetInnerHTML={{ __html: html }}
+            className="blog-content"
+          />
+
+          <Engagement slug={post.slug} />
+        </div>
+
+        <BlogAside post={post} />
+      </section>
+
+      <Link
+        href="/blog"
+        className="mt-20 md:mt-30 weak-text weak-hover flex items-center gap-1"
+      >
+        <ChevronLeft size={16} /> More posts
+      </Link>
+    </article>
+  )
+}
