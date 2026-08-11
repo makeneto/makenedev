1e003ef20ff817acb125ea68c0e6435386865883
Makene Neto <makeneto17@gmail.com>
2026-08-11 22:33:38 +0100

    chore: add masterdriver project

--- src/data/works.ts
+++ src/data/works.ts
@@ -2,43 +2,34 @@ import type { ShowcaseItem } from "../interfaces/showcase"
 
 export const works: ShowcaseItem[] = [
   {
+    id: 109,
+    title: "Masterdriver",
+    description:
+      "Study for your driving test like it's a game. Earn XP and learn to drive at your own pace.",
+    imageUrl: "/assets/work-cover9.webp",
+    link: "https://masterdriver.vercel.app",
+  },
+  {
     id: 108,
-    slug: "eminus",
-    created_at: "2026-05-10",
     title: "Eminus",
     description:
       "Building better financial habits for families with smarter money management.",
     imageUrl: "/assets/work-cover8.webp",
-    href: "https://github.com/makeneto/eminus",
-    stacks: [
-      "React.js",
-      "TypeScript",
-      "Tailwindcss",
-      "Shadcn/ui",
-      "Sass",
-      "Recharts",
-      "Lucide Icons",
-    ],
+    link: "https://github.com/makeneto/eminus",
   },
   {
     id: 102,
-    slug: "beats-by-dre",
-    created_at: "2025-09-10",
     title: "Beats by Dre",
     description:
       "Exclusive collection of premium headphones with immersive sound.",
     imageUrl: "/assets/work-cover2.webp",
-    href: "https://beats-by-dr.vercel.app",
-    stacks: ["Next.js", "TypeScript", "Tailwindcss", "Sass", "Lucide Icons"],
+    link: "https://beats-by-dr.vercel.app",
   },
   {
     id: 106,
-    created_at: "2023-08-10",
-    slug: "wigland",
     title: "Wigland",
     description: "Find incredible wigs to enhance your beauty and style.",
     imageUrl: "/assets/work-cover7.webp",
-    href: "https://perucalandia.vercel.app",
-    stacks: ["HTML5", "CSS3", "JavaScript"],
+    link: "https://perucalandia.vercel.app",
   },
 ]
