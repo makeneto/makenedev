04bd2ee25144656a96bfdb0e5aff9bc3489d1665
Makene Neto <makeneto17@gmail.com>
2026-08-12 11:12:32 +0100

    feat: apply customized position for tooltip component

--- src/components/ui/ReusableTooltip.tsx
+++ src/components/ui/ReusableTooltip.tsx
@@ -3,17 +3,19 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip"
 
 interface ReusableTooltipProps {
   content: string
+  side?: "top" | "right" | "bottom" | "left" | undefined
   children: ReactNode
 }
 
 export default function ReusableTooltip({
   content,
+  side,
   children,
 }: ReusableTooltipProps) {
   return (
     <Tooltip>
       <TooltipTrigger asChild>{children}</TooltipTrigger>
-      <TooltipContent>
+      <TooltipContent side={side ?? undefined}>
         <p>{content}</p>
       </TooltipContent>
     </Tooltip>
