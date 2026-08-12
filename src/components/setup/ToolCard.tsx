4a734b5ef901145398bee79a9cbbec3c8d4b0b66
Makene Neto <makeneto17@gmail.com>
2026-08-12 23:45:21 +0100

    chore(gears)): replace Link to li component

--- src/components/setup/ToolCard.tsx
+++ src/components/setup/ToolCard.tsx
@@ -1,37 +1,17 @@
-import { ExternalLink } from "lucide-react"
+import { BlogPost, coverImage } from "@/services/wisp"
 import Image from "next/image"
-import Link from "next/link"
 
-interface ToolCardProps {
-  image: string
-  name: string
-  description: string
-  link: string
-}
+export default function ToolCard({ post }: { post: BlogPost }) {
+  const { image, title, description } = post
 
-export default function ToolCard({
-  image,
-  name,
-  description,
-  link,
-}: ToolCardProps) {
   return (
-    <Link
-      className="setup-card"
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Visit external link"
-    >
-      <Image src={image} alt={name} width={100} height={100} />
+    <li className="setup-card">
+      <Image src={coverImage(image)} alt={title} width={100} height={100} />
 
-      <section className="setup-card__content">
-        <div>
-          <h3>{name}</h3>
-          <ExternalLink />
-        </div>
+      <div className="setup-card__content">
+        <h3>{title}</h3>
         <p>{description}</p>
-      </section>
-    </Link>
+      </div>
+    </li>
   )
 }
