d7480fd1258c190e8c0c198adf7589ba0cdb5366
Makene Neto <makeneto17@gmail.com>
2026-08-12 16:51:29 +0100

    style(checkbox): update checkbox background

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -12,7 +12,7 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer relative flex size-4 shrink-0 items-center justify-center rounded-[4px] border border-zinc-400 dark:border-input transition-colors outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 aria-invalid:aria-checked:border-primary dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 data-checked:border-0 data-checked:bg-black data-checked:text-primary-foreground dark:data-checked:bg-white",
+        "peer relative flex size-4 shrink-0 items-center justify-center rounded-[4px] border border-zinc-400 dark:border-input transition-colors outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 aria-invalid:aria-checked:border-primary dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 data-checked:border-0 data-checked:bg-foreground data-checked:text-primary-foreground dark:data-checked:bg-white",
         className,
       )}
       {...props}
@@ -21,7 +21,7 @@ function Checkbox({
         data-slot="checkbox-indicator"
         className="grid place-content-center text-current transition-none [&>svg]:size-3.5"
       >
-        <CheckIcon className="white dark:black" />
+        <CheckIcon className="mt-px white dark:black" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   )
