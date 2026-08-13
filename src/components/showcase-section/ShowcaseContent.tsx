963ef3c8ddb773ddb91e99d2904e700276c1511f
Makene Neto <makeneto17@gmail.com>
2026-08-13 15:35:07 +0100

    develop: showcase content

---
+++ src/components/showcase-section/ShowcaseContent.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import ShowcaseHeader from "./ShowcaseHeader"
+import ShowcaseGrid from "./ShowcaseGrid"
+import SectionControls from "../ui/SectionControls"
+import { useShowcasePagination } from "@/hooks/useShowcasePagination"
+import { ShowcaseContentProps } from "@/interfaces/showcase"
+
+export default function ShowcaseContent({
+  posts,
+  title,
+  viewAll,
+  isHomePage,
+  isShowcase,
+  count,
+}: ShowcaseContentProps) {
+  const {
+    sectionRef,
+    page,
+    totalPages,
+    visiblePosts,
+    handleNext,
+    handlePrev,
+    showControls,
+  } = useShowcasePagination({
+    posts,
+    isHomePage,
+  })
+
+  return (
+    <section ref={sectionRef}>
+      <ShowcaseHeader
+        title={title}
+        count={count ? count : posts.length}
+        linkSection={viewAll}
+        isShowcase={isShowcase}
+      />
+
+      <ShowcaseGrid posts={visiblePosts} />
+
+      {showControls && !viewAll && (
+        <SectionControls
+          page={page}
+          totalPages={totalPages}
+          viewAll={viewAll}
+          onPrev={handlePrev}
+          onNext={handleNext}
+        />
+      )}
+    </section>
+  )
+}
