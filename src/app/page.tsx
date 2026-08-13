82c8fc5d613054760295504c07e8158134194c0d
Makene Neto <makeneto17@gmail.com>
2026-08-13 23:42:04 +0100

    feat(seo): add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Metadata } from "next"
 
 import HomeHeader from "@/components/headers/HomeHeader"
 import ShowcaseSection from "@/components/showcase-section/ShowcaseSection"
@@ -7,6 +8,23 @@ import PricingSection from "@/components/pricing/PricingSection"
 import BlogArea from "@/components/blog/BlogArea"
 import LastBlog from "@/components/blog/LastBlog"
 import VerticalBlogList from "@/components/blog/VerticalBlogList"
+import { SITE_URL } from "@/constants/siteUrl"
+
+export const metadata: Metadata = {
+  title: "Makene Neto",
+  description:
+    "Makene Neto — Software Engineer. Minimalist portfolio showcasing projects, experiences, and what motivates me as a software engineer.",
+  alternates: {
+    canonical: SITE_URL,
+  },
+  openGraph: {
+    title: "My Works | Makene Neto",
+    description:
+      "Makene Neto — Software Engineer. Minimalist portfolio showcasing projects, experiences, and what motivates me as a software engineer.",
+    url: SITE_URL,
+    type: "website",
+  },
+}
 
 export default async function HomePage() {
   return (
