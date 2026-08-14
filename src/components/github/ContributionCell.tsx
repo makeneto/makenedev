4a4ed7e1eadeadcde3a00e9578c0f0deecfa08fe
Makene Neto <makeneto17@gmail.com>
2026-08-14 13:47:04 +0100

    chore: rename githubTypes to github-types import

--- src/components/github/ContributionCell.tsx
+++ src/components/github/ContributionCell.tsx
@@ -3,10 +3,10 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-import { ContributionCellProps } from "@/interfaces/githubTypes"
+import { ContributionCellProps } from "@/interfaces/github-types"
 import { formatShortDate } from "@/utils/formatDate"
+import { formatContributionDate } from "@/utils/github/formatContributionDate"
 import { getContributionLevel } from "@/utils/github/getContributionLevel"
-import { formatContributionDate } from "@/utils/github/getMaxContributionCount"
 
 export function ContributionCell({ day, max }: ContributionCellProps) {
   const level = getContributionLevel(day.contributionCount, max)
