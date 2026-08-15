b60a45ea8a74f3845b299ac2390fe676135fbf1e
Makene Neto <makeneto17@gmail.com>
2026-08-15 15:03:03 +0100

    chore: rename BookResume file to BooksResume

---
+++ src/components/books/BooksResume.tsx
@@ -0,0 +1,13 @@
+import ResumeFrame from "../ResumeFrame"
+
+export default function BooksResume() {
+  return (
+    <ResumeFrame>
+      <p className="description--medium">
+        If you have a recommendation that you think should be on this list, send
+        me a message. I'm always looking for the next book that will mess with
+        me.
+      </p>
+    </ResumeFrame>
+  )
+}
