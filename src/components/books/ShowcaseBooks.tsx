95f2ad85a28d2b1646a0675fb5502235a531db0c
Makene Neto <makeneto17@gmail.com>
2026-08-15 15:03:17 +0100

    refactor: develop Showcase Books section

---
+++ src/components/books/ShowcaseBooks.tsx
@@ -0,0 +1,19 @@
+import { getBooksHomeData } from "@/features/books/wispBooks"
+import ShowcaseHeader from "../showcase-section/ShowcaseHeader"
+import BookCard from "./BookCard"
+
+export default async function ShowcaseBooks() {
+  const { posts } = await getBooksHomeData()
+
+  return (
+    <section>
+      <ShowcaseHeader title="Bookcase" count={posts.length} />
+
+      <ul className="book-list">
+        {posts.map((book) => (
+          <BookCard book={book} />
+        ))}
+      </ul>
+    </section>
+  )
+}
