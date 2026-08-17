f88b47eaa1502963e1e4e26d44340be72e7ee8db
Makene Neto <makeneto17@gmail.com>
2026-08-17 20:45:33 +0100

    style  add sticky relative in PostAside

--- src/components/post/PostAside.tsx
+++ src/components/post/PostAside.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
 
 export default function PostAside({ children }: { children: ReactNode }) {
-  return <aside className="post-aside">{children}</aside>
+  return <aside className="post-aside relative">{children}</aside>
 }
