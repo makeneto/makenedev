9bc90ede44424a5a7608453228ac75836dfc2e38
Makene Neto <makeneto17@gmail.com>
2026-08-18 16:41:54 +0100

    feat: develop blogHeader

---
+++ src/components/blog/BlogHeader.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image"
+import Link from "next/link"
+import { RiArrowRightSLine } from "@remixicon/react"
+import { CalendarDays } from "lucide-react"
+
+import { site } from "@/constants/site"
+import { formatLongDate } from "@/utils/formatDate"
+import { BlogArticle } from "@/services/wisp"
+
+export default function BlogHeader({ post }: { post: BlogArticle }) {
+  const { publishedAt, createdAt } = post
+  const date = publishedAt || createdAt
+
+  return (
+    <header className="grid gap-5 sm:gap-5">
+      <span className="flex items-center gap-1">
+        <Link href="/blog">Blog</Link>
+        <RiArrowRightSLine size={16} />
+        <h1 className="line weak-text line-clamp-1">{post.title}</h1>
+      </span>
+
+      <div className="post-intro">
+        <h1>{post.title}</h1>
+
+        <div className="flex items-center gap-3 text-sm">
+          <Link href="/about" className="flex items-center gap-2">
+            <Image
+              src={`${site.github}.png`}
+              alt="Makene's profile"
+              width={18}
+              height={18}
+              className="rounded-full"
+            />
+            <span className="font-medium">{site.dev}</span>
+          </Link>
+
+          <div className="w-px h-5 bg-zinc-200 dark:bg-zinc-800" />
+
+          <span className="weak-text flex items-center gap-2">
+            <CalendarDays size={14} /> {date ? formatLongDate(date) : "No data"}
+          </span>
+        </div>
+
+        <p className="sm:text-[1.04rem] xl:text-[1.2rem] mt-12">
+          {post.description}
+        </p>
+      </div>
+    </header>
+  )
+}
