fa4c8fea98051b3a1becd6a2245c33a77ad6532c
Makene Neto <makeneto17@gmail.com>
2026-08-18 16:38:47 +0100

    style: organize the ArticleContent layout

--- src/components/blog/ArticleContent.tsx
+++ src/components/blog/ArticleContent.tsx
@@ -3,11 +3,11 @@
 import { useMemo } from "react"
 
 import BlogAside from "./BlogAside"
-import PostHeader from "../post/PostHeader"
 import { processBlogContent } from "@/utils/processBlogContent"
 import { Engagement } from "./Engagement"
 import { Controls } from "../post/PostControls"
 import { PostContentProps } from "@/interfaces/post"
+import BlogHeader from "./BlogHeader"
 
 export function ArticleContent({ post, previous, next }: PostContentProps) {
   const { html } = useMemo(
@@ -16,28 +16,26 @@ export function ArticleContent({ post, previous, next }: PostContentProps) {
   )
 
   return (
-    <article>
-      <PostHeader post={post} variant="blog" />
+    <article className="relative grid sm:grid-cols-[70%_auto] gap-20 xl:gap-15">
+      <div className="relative">
+        <BlogHeader post={post} />
 
-      <section className="mt-20 relative grid sm:grid-cols-[70%_auto] gap-20 xl:gap-15">
-        <div className="relative">
-          <div
-            dangerouslySetInnerHTML={{ __html: html }}
-            className="post-content blog-content"
-          />
+        <div
+          dangerouslySetInnerHTML={{ __html: html }}
+          className="mt-20 post-content blog-content"
+        />
 
-          <Engagement post={post} basePath="blog" />
+        <Engagement post={post} basePath="blog" />
 
-          <Controls
-            variant="blog"
-            previous={previous}
-            next={next}
-            basePath="/blog"
-          />
-        </div>
+        <Controls
+          variant="blog"
+          previous={previous}
+          next={next}
+          basePath="/blog"
+        />
+      </div>
 
-        <BlogAside post={post} />
-      </section>
+      <BlogAside post={post} className="mt-15" />
     </article>
   )
 }
