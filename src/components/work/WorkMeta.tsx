ed39f9110466bce76884d53258284d1f9f4267ba
Makene Neto <makeneto17@gmail.com>
2026-08-26 17:18:27 +0100

    refactor: separate Makene's profile link and create a component

--- src/components/work/WorkMeta.tsx
+++ src/components/work/WorkMeta.tsx
@@ -1,26 +1,10 @@
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
-
-import { site } from "@/constants/site"
+import MakeneProfileLink from "../post/MakeneProfileLink"
 
 export function WorkMeta() {
   return (
-    <React.Fragment>
-      <div>
-        <p>Contributors</p>
-
-        <Link href="/about" className="flex items-center gap-2">
-          <Image
-            src={`${site.github}.png`}
-            alt="Makene's profile"
-            width={20}
-            height={20}
-            className="rounded-full"
-          />
-          <p>{site.dev}</p>
-        </Link>
-      </div>
-    </React.Fragment>
+    <div>
+      <p>Contributors</p>
+      <MakeneProfileLink />
+    </div>
   )
 }
