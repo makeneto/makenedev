804e8cb9045692120af9dda667b322d07f019d5c
Makene Neto <makeneto17@gmail.com>
2026-08-26 13:19:36 +0100

    feat(pricing-button): add contact link options

--- src/components/pricing/PricingButton.tsx
+++ src/components/pricing/PricingButton.tsx
@@ -2,17 +2,22 @@ import Link from "next/link"
 import { RiArrowRightSLine } from "@remixicon/react"
 
 interface PricingButtonProps {
+  link: string
+  externalLink?: boolean
   highlighted?: boolean
   buttonText: string
 }
 
 export default function PricingButton({
+  link,
+  externalLink = false,
   highlighted,
   buttonText,
 }: PricingButtonProps) {
   return (
     <Link
-      href="/contact"
+      href={link}
+      target={externalLink ? "_blank" : "_self"}
       className={`pricing__button ${highlighted ? "pricing__button--primary" : ""}`}
       aria-label="Get started Now"
     >
